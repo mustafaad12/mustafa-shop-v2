b9ec4fdf39fb13001cb0f044239dba74995300a8
unknown <mustafa.ssi.msn121@gmail.com>
2023-06-02 16:22:47 +0300

    change header css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 //font awesome icons
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="md">
+      <Navbar className="header-nav" variant="dark" expand="md">
         <Container>
-          <Navbar.Brand href="#home">Mustafa Shop</Navbar.Brand>
+          <Navbar.Brand className="header-logo" href="#home">
+            <img src={logo} alt="logo" />
+            Mustafa Shop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">
+              <Nav.Link className="header-links" href="#home">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="#link">
+              <Nav.Link className="header-links" href="#link">
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
