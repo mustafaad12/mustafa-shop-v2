c89efaabe970364d9d9ea2efd7ab01ec21509fdf
unknown <mustafa.ssi.msn121@gmail.com>
2023-06-10 19:17:08 +0300

    conect mongoose to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 import express from "express";
+
 import dotenv from "dotenv";
 dotenv.config();
+
 import cors from "cors";
+
+import { connectDB } from "./config/db.js";
+
 import { ProductController } from "./controllers/productController.js";
+
+connectDB();
 
 const app = express();
 
