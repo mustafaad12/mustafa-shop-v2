35f0ab67fa1ae8154b2293b50808cf82865e1129
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2023-06-15 23:33:44 +0300

    setup redux store and api slice using RTK Query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import store from "./store.js";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/index.css";
 import {
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
