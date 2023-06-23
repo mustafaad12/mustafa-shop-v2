39383b1554f601b1f6883342c523d47b19daa900
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2023-06-23 17:57:41 +0300

    auth slice & users Api slice

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (id, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    maxAge: 3 * 24 * 60 * 60 * 1000,
   });
 };
 
