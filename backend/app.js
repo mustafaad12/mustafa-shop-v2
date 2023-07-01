4a2fdbabdad946cb85ce74b2bbb34ae06953f37f
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2023-07-01 22:13:07 +0300

    add proxy

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
-import cors from "cors";
-
 import { connectDB } from "./config/db.js";
 
 import { ProductController } from "./controllers/productController.js";
@@ -16,7 +14,6 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
