cf1f6da1b396da51ce1a19d989366fbe2bd4613c
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2023-07-10 18:38:28 +0300

    private route component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,7 @@ import ProductScreen from "./screens/ProductScreen.jsx";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
 
 const router = createBrowserRouter(
@@ -27,7 +28,9 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
