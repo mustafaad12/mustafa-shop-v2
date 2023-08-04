9ef247d8feb5c85c1fde2d18f48d87c258f82a1f
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2023-08-05 00:29:33 +0300

    preparing to deploy

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", new ProductController().router);
 app.use("/api/users", new UserController().router);
 app.use("/api/orders", new OrderController().router);
@@ -32,6 +28,20 @@ app.use("/api/upload", uploadController);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  //any router that is not api will be redirected to index.js
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use([notFound, errorHandler]);
 
--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connect = mongoose.connect("mongodb://127.0.0.1:27017/proshop-v2");
+    const connect = mongoose.connect(
+      "mongodb+srv://mustafaAldaffaie:qweqweqwe123123@cluster0.badzx.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log(`Mongodb Connected`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
